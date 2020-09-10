0a74b0eebb9431457ced5a81342bdc3ed2026952
Hurly77 <cameron.leverett@yahoo.com>
2020-09-10 14:44:18 -0700

    Done.

--- index.js
+++ index.js
@@ -16,20 +16,18 @@ class Polygon {
 
 class Triangle extends Polygon {
 	get isValid() {
-    let [ a, b, c ] = this.sidesArray
-    return (a + b <= c || b + c <= a || a + c <= b) ? false : true;
+		let [ a, b, c ] = this.sidesArray;
+		return a + b <= c || b + c <= a || a + c <= b ? false : true;
 	}
 }
 
 class Square extends Polygon {
 	get isValid() {
-    let [a, b, c, d] = this.sidesArray
-   return (a && b && c == d) ? true : false
-  }
+		let [ a, b, c, d ] = this.sidesArray;
+		return a && b && c == d ? true : false;
+	}
 
 	get area() {
-    return this.sidesArray[0]**2
-  }
+		return this.sidesArray[0] ** 2;
+	}
 }
-let three = new Triangle([ 1, 2, 3 ]);
-console.log(three.isValid);
